871fe272a845724681e118022beccf2cc5485d0d
Kirill Tuchkin <volkirtu@gmail.com>
2023-03-04 16:26:14 +0300

    coursework 4

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from flask_restx import Api
 from configs.config import Config
 from configs.config_db import db
 from utils.create_and_load_data import create_and_load_data
-from views.auth import auth_ns
 from views.directors import directors_ns
+
 from views.genres import genres_ns
 from views.movies import movies_ns
+from views.auth import auth_ns
 from views.users import users_ns
 
 
 def create_app(config):
     application = Flask(__name__)
     application.config.from_object(config)
+
     application.app_context().push()
 
     return application
@@ -30,10 +32,11 @@ def configure_app(application):
     api.add_namespace(auth_ns)
 
 
-app_config = Config()
-app = create_app(app_config)
-configure_app(app)
-create_and_load_data()
 
 if __name__ == '__main__':
+    app_config = Config()
+    app = create_app(app_config)
+    configure_app(app)
+    create_and_load_data()
+
     app.run()
