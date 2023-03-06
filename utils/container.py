9fbf86f67b93c9ed286d20f067f873c9edc7cbfa
Kirill Tuchkin <volkirtu@gmail.com>
2023-03-06 14:27:18 +0300

    new service and dao connections added

--- utils/container.py
+++ utils/container.py
@@ -1,10 +1,12 @@
 from dao.auth import AuthDAO
 from dao.director import DirectorDAO
+from dao.favorite import FavoriteDAO
 from dao.genre import GenreDAO
 from dao.movie import MovieDAO
 from dao.user import UserDAO
 from service.auth import AuthService
 from service.director import DirectorService
+from service.favorite import FavoriteService
 from service.genre import GenreService
 from service.movie import MovieService
 from configs.config_db import db
@@ -15,10 +17,12 @@ genre_dao = GenreDAO(db.session)
 movie_dao = MovieDAO(db.session)
 auth_dao = AuthDAO(db.session)
 user_dao = UserDAO(db.session)
+favorite_dao = FavoriteDAO(db.session)
 
 director_service = DirectorService(director_dao)
 genre_service = GenreService(genre_dao)
 movie_service = MovieService(movie_dao)
 auth_service = AuthService(auth_dao)
 user_service = UserService(user_dao)
+favorite_service = FavoriteService(favorite_dao)
 
