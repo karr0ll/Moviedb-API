1aa2b7ffe0b1eb6fe194b3531055930b16ab0989
Kirill Tuchkin <volkirtu@gmail.com>
2023-03-06 14:26:33 +0300

    update_user() and update_password() fucntiones fixed

--- service/user.py
+++ service/user.py
@@ -2,24 +2,55 @@ import base64
 import hashlib
 import hmac
 
+import jwt
 from flask import abort
 
 from dao.user import UserDAO
-from utils.constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS
+from utils.constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS, JWT_SECRET, JWT_ALGORITHM
 
 
 class UserService:
     def __init__(self, dao: UserDAO):
         self.dao = dao
 
-    def get_one(self, uid):
+    def update_user(self, user_data, data):
+        email = self.get_user_email_from_token(user_data)
+        uid = self.dao.get_user_id_by_email(email)
+        return self.dao.update_user(data, uid)
+
+    def update_password(self, token_data, user_data):
+        """
+        token_data: данные пользователя из токена, получаемые через заголовок Authorization
+        user_data: данные из json, получаемые в body запроса
+        """
+
+        email = self.get_user_email_from_token(token_data)
+        uid = self.dao.get_user_id_by_email(email)
+        print(uid)
+        db_password_hashed = self.dao.get_current_hashed_password_by_id(uid)
+        entered_password = user_data["password_1"]
+        new_password = user_data["password_2"]
+
+        if self.compare_passwords(db_password_hashed, entered_password):
+            new_password_hashed = self.generate_hashed_password(new_password)
+            return self.dao.update_password(new_password_hashed, email)
+
+        if not self.compare_passwords(db_password_hashed, entered_password):
+            raise abort(400)
+
+    def get_user_email_from_token(self, data):
+        token_data = data.split("Bearer ")[-1]
+        data_from_token = jwt.decode(token_data, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        email = data_from_token.get("email")
+
+        return email
+
+    def get_one(self, data):
+        email = self.get_user_email_from_token(data)
+        uid = self.dao.get_user_id_by_email(email)
         data = self.dao.get_one(uid)
 
         return data
-
-    def update_user(self, user_data):
-        user_data["password"] = self.generate_hashed_password(user_data["password"])
-        return self.dao.update_user(user_data)
 
     def generate_hashed_password(self, password):
         hash_pass = hashlib.pbkdf2_hmac(
@@ -34,20 +65,3 @@ class UserService:
 
         hash_pass = self.generate_hashed_password(entered_password)
         return hmac.compare_digest(db_password_hashed, hash_pass)
-
-    def update_password(self, user_data):
-        entered_password = user_data["password"]
-        db_password_hashed = self.dao.get_current_password_hashed_by_id(user_data["id"])
-
-        if self.compare_passwords(db_password_hashed, entered_password):
-            new_password_hashed = self.generate_hashed_password(user_data["new_password"])
-            return self.dao.update_password(user_data, new_password_hashed)
-
-        if not self.compare_passwords(db_password_hashed, entered_password):
-            raise abort(400)
-
-
-
-
-
-
