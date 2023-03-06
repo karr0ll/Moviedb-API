a9b8c5a10a34bceb69d190e6dbf8b11bb5413353
Kirill Tuchkin <volkirtu@gmail.com>
2023-03-06 14:30:53 +0300

    endpoints fixes

--- views/users.py
+++ views/users.py
@@ -8,30 +8,40 @@ from utils.decorator import auth_required
 users_ns = Namespace("user")
 
 
-@users_ns.route("/<int:uid>")
+@users_ns.route("/")
 class UsersViews(Resource):
     @auth_required
-    def get(self, uid):
-        user_data = user_service.get_one(uid)
+    def get(self):
+        """
+        Получает данные пользователя из заголовка и
+        возвращает данные пользователя из базы
+        """
+        data = request.headers["Authorization"]
+        user_data = user_service.get_one(data)
         return user_schema.dump(user_data), 200
 
     @auth_required
-    def patch(self, uid):
+    def patch(self):
+        """
+        Получает данные пользователя из заголовка и новые данные пользователя,
+        и обновляет данные пользователя в базе
+        """
+        token_data = request.headers["Authorization"]
         user_data = request.json
-        user_data["id"] = uid
         try:
-            user_service.update_user(user_data)
+            user_service.update_user(token_data, user_data)
             return "", 201
         except Exception as e:
-            return str(e), 404
+            return str(e), 401
 
 @users_ns.route("/password/")
 class UserViews(Resource):
     @auth_required
     def put(self):
-        data = request.json
+        data = request.headers["Authorization"]
+        user_data = request.json
         try:
-            user_service.update_password(data)
+            user_service.update_password(data, user_data)
             return "", 201
         except Exception as e:
-            return str(e), 404
+            return str(e), 401
