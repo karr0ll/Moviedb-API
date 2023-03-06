1ce2504e9e9388f999492dee79636920dd46ffb9
Kirill Tuchkin <volkirtu@gmail.com>
2023-03-06 14:23:53 +0300

    foxed update_user(), update_password() functions get_user_id_by_email(), get_current_hashed_password_by_id() functions added

--- dao/user.py
+++ dao/user.py
@@ -15,37 +15,30 @@ class UserDAO:
             User.surname,
             User.favorite_genre
         ).filter(User.id == uid).first()
-
         return user_data
 
-    def update_user(self, user_data):
-        uid = user_data.get("id")
-        user = self.get_one(uid)
-        user.id = uid
-        user.email = user_data.get("email")
-        user.password = user_data.get("password")
-        user.name = user_data.get("name")
-        user.surname = user_data.get("surname")
-        user.favorite_genre = user_data.get("favorite_genre")
+    def update_user(self, data, uid):
+        user = self.session.query(User).filter(User.id == uid).first()
+        user.name = data.get("name")
+        user.surname = data.get("surname")
+        user.favorite_genre = data.get("favorite_genre")
         self.session.add(user)
         self.session.commit()
 
-    def get_current_password_hashed_by_id(self, uid):
+    def update_password(self, new_password_hashed, email):
+        uid = self.get_user_id_by_email(email)
+        user = self.session.query(User).filter(User.id == uid).first()
+        user.password = new_password_hashed
+        self.session.add(user)
+        self.session.commit()
+
+    def get_user_id_by_email(self, email):
+        uid = self.session.query(User.id).filter(User.email == email)
+        print(uid)
+        return uid
+
+    def get_current_hashed_password_by_id(self, uid):
         current_password = self.session.query(User.password).filter(User.id == uid).first()
 
         for password in current_password:
             return password
-
-    def update_password(self, user_data, new_password_hashed):
-        uid = user_data.get("id")
-        user = self.session.query(User).get(uid)
-        user.id = uid
-        user.email = user_data.get("email")
-        user.password = new_password_hashed
-        user.name = user_data.get("name", None)
-        user.surname = user_data.get("surname", None)
-        user.favorite_genre = user_data.get("favorite_genre", None)
-        self.session.add(user)
-        self.session.commit()
-
-
