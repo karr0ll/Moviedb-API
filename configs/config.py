1b3a8fb495bad60b72f0b5e0d589a143f0f33386
Kirill Tuchkin <volkirtu@gmail.com>
2023-03-08 18:55:00 +0300

    minor fixes

--- configs/config.py
+++ configs/config.py
@@ -2,4 +2,12 @@ class Config:
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    AUTHORIZATIONS = {
+        'apikey': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'X-API-KEY'
+        }
+    }
+
 
